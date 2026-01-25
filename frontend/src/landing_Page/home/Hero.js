c1263caccd6aa0fa1a4a2de47f8e8ed811e0bb3d
Aman kumawat <amankumawat4507@gmail.com>
2026-01-25 15:43:46 +0500

    UI changes

--- frontend/src/landing_Page/home/Hero.js
+++ frontend/src/landing_Page/home/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='container text-center'>
       <div className='row'>
-          <img src='media/Images/homeHero4.png' alt='Hero Image' style={{width:"100%", height:"90%",objectFit:"contain"}}/>
+          <img src='media/Images/HomeHero4.png' alt='Hero Image' style={{width:"100%", height:"90%",objectFit:"contain"}}/>
           <h1 className='mt-5'>Invest in everything</h1>
           <p>Online platform to invert in Stocks, derivatives, mutual funds, and more</p>
           <button className='btn btn-primary p-2 fs-5 mt-4 mb-5 signupBtn'>Signup Now</button>
